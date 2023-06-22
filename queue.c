2670a86585516981fa4dbfc48b3089b2782be8b0
sana-ez <sanaezzahid@gmail.com>
2023-06-22 09:53:58 -0700

    nop.c

---
+++ queue.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+/**
+ * f_queue - PRINTS THE TOP
+ * @head: STACK HEAD
+ * @counter: LINE_NUMBER
+ * return: NO RETURN
+*/
+void f_queue(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 1;
+}
+
+/**
+ * addqueue - ADD NODE TO THE TAIL STACK
+ * @n: NEW_VALUE
+ * @head: HEAD OF THE STACK
+ * return: NO RETURN
+*/
+void addqueue(stack_t **head, int n)
+{
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (aux)
+	{
+		while (aux->next)
+			aux = aux->next;
+	}
+	if (!aux)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		aux->next = new_node;
+		new_node->prev = aux;
+	}
+}
---
+++ stack.C
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - PRINTS THE TOP
+ * @head: STACK HEAD
+ * @counter: LINE_NUMBER
+ * return: NO RETURN
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
